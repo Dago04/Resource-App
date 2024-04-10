49c57082282e224d6d6ae0762f439f14b9cd0064
Diegocarque12 <diegocarque@hotmail.com>
2024-04-10 17:58:14 -0600

    create search main component

---
+++ src/components/SearchBar/SearchBar.jsx
@@ -0,0 +1,15 @@
+import Button from "./Button";
+import SearchCategories from "./SearchCategories";
+import SearchWords from "./SearchWords";
+
+const SearchBar = () => {
+  return (
+    <>
+      <SearchCategories />
+      <SearchWords />
+      <Button />
+    </>
+  );
+};
+
+export default SearchBar;
