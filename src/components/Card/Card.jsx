ba1ab327afe092f30f7f020b1bbdd0894266f88e
Dagoberto Salas <DagoSalas1999@gmail.com>
2024-04-10 18:28:38 -0600

    WIP Card

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -1,7 +1,7 @@
 import { CardItem } from "./CardItem"
 export const Card = () => {
     return (
-        <section className="w-full flex p-5 flex-wrap gap-2 ">
+        <section className="w-full  flex flex-col p-8 lg:grid lg:grid-cols-2 place-items-center gap-3 mt-12">
 
             <CardItem />
         </section>
